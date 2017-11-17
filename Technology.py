b0c0127838a9e4a0ea669e52d0cf40e5eae49a57
Adebola.Oshomoji <adebola.oshomoji@keyrus.com>
2017-11-17 15:53:35 +0000

    Comment

--- Technology.py
+++ Technology.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import requests
+import lxml
+from pytrends.request import TrendReq
 
 def set_working_directory(directory):
     '''Reads directory as a string argument and changes to desired location'''
@@ -20,8 +23,44 @@ def create_unique_list(dataframe, column, sort=False):
         unique = sorted(unique, key=str.lower)
     return unique
 
+def query_trends(kwlist, timeframe='today 5-y', geo=''):
+    '''
+    Returns Google trends interest over time data for specified time period and location.
+    kwlist: Key-word as a list
+    timeframe: e.g. "today 5-m" - Default = last 5 years ("today 5-y")
+    geo: e.g. "US" - Default = World ('')
+    '''
+    pytrends.build_payload(kwlist, timeframe=timeframe, geo=geo)
+    df = pytrends.interest_over_time()
+    return df
+
+def create_trend_dataframe(dataframe):
+    trends = pd.DataFrame()
+    empty = []
+    if len(df) == 0:
+        empty.append()
+
 set_working_directory(r'C:\Users\adebola.oshomoji\Documents\KEYRUS_Bootcamp_November\2017\20171030_erp_be_unzipped')
 df = readCSV('technology.csv', sep = ',', encoding='utf-16', error_bad_lines=False)
 technology =  create_unique_list(df, 'Name', sort=True)
 
-print([tech for tech in technology])
+pytrends = TrendReq(hl='en-US', tz=360)
+
+tech_interest = pd.DataFrame()
+
+kw_list = technology[4:6]
+pytrends.build_payload('test', timeframe='today 5-y') #
+df = pytrends.interest_over_time() #
+print(df)
+print(technology[5])
+#print(len(technology) - 1)
+'''
+for tech in technology[0]:
+    kw_list = [tech]
+    pytrends.build_payload(kw_list, timeframe='today 5-y')
+    df = pytrends.interest_over_time()
+    df = df.drop('isPartial', axis=1)
+    tech_interest = pd.concat([tech_interest, df], axis=1)
+
+print(tech_interest)
+'''
