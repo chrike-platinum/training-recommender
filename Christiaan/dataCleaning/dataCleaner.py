767b354142256b31cb8c436ae6a8e9494d13b0c6
christiaan <christiaanleysen@gmail.com>
2017-11-29 14:54:06 +0100

    data word cleaning

--- Christiaan/dataCleaning/dataCleaner.py
+++ Christiaan/dataCleaning/dataCleaner.py
@@ -3,11 +3,28 @@ __author__ = 'christiaan'
 from Christiaan import DataFrameLoader
 
 def cleanTechWord(techWord):
-    TechWordSplit = techWord.lower().replace('(',' ').replace(')',' ').replace('/',' ').split(' ')
+    TechWordSplit = techWord.lower().replace('.',' ').replace('(',' ').replace(')',' ').replace('/',' ').split(' ')
     if TechWordSplit[0]=='microsoft' or TechWordSplit[0]=='ms':
-        return TechWordSplit[1]+'(ms)'
-    elif  TechWordSplit[0]=='bo':
-        return 'sap'
+        if TechWordSplit[1]=='visual':
+            return TechWordSplit[0]+' '+TechWordSplit[1]+' (ms)'
+        else:
+            return TechWordSplit[1]#+' (ms)'
+    elif  TechWordSplit[0]=='bo' or TechWordSplit[0]=='business' and TechWordSplit[1]=='objects':
+        return 'sap'#TechWordSplit[1]+' (sap)'
+    elif  TechWordSplit[0]=='complete':
+        return TechWordSplit[1]
+    elif  TechWordSplit[0]=='visual':
+        return TechWordSplit[0]+' '+TechWordSplit[1]
+    elif  TechWordSplit[0]=='machine':
+        return TechWordSplit[0]+' '+TechWordSplit[1]
+    elif  TechWordSplit[0]=='big':
+        return TechWordSplit[0]+' '+TechWordSplit[1]
+    elif TechWordSplit[0]=='oracle':
+        return TechWordSplit[1]#+' (oracle)'
+    elif TechWordSplit[0]=='open':
+        return TechWordSplit[0]+' '+TechWordSplit[1]
+    elif TechWordSplit[0]=='power':
+        return TechWordSplit[0]+' '+TechWordSplit[1]
     else:
         return TechWordSplit[0]
 
@@ -22,7 +39,7 @@ def getExperienceTechDataSet():
     df = df[['Employee Number','Name','Name (dim levelsofexperience.csv)','Name (dim technologycategories.csv)']]
     df.columns = ['employeeNumber','Technology','ExperienceLevel','TechnologyCategory']
     df = df.sort_values('employeeNumber')
-    df = cleanTechColumn(df,'Technology')
+    df = cleanTechColumn(df,'Technology')#Data cleaning
     return df
 
 def getListOfTechnologiesAndCats(df,employeeNumber,employeeNrColumnName,technologyColumnName,technologyCatColumnName):
@@ -46,7 +63,12 @@ def getListOfTechnologies(df,employeeNumber,employeeNrColumnName,technologyColum
 def getUniqueValuesInColumn(df,columnName):
     return df[columnName].unique()
 
-df = getExperienceTechDataSet()
-employeeNumbers = getUniqueValuesInColumn(df,'employeeNumber')
-for item in employeeNumbers:
-    print((item,getListOfTechnologies(df,item,'employeeNumber','Technology','TechnologyCategory')))
+
+def getTechFromExperience():
+    df = getExperienceTechDataSet()
+    employeeNumbers = getUniqueValuesInColumn(df, 'employeeNumber')
+    for item in employeeNumbers:
+        print((item, getListOfTechnologies(df, item, 'employeeNumber', 'Technology', 'TechnologyCategory')))
+
+
+getTechFromExperience()
